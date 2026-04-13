b35d1ad6f27e51e44b4e477bf40f7ffa87357506
Water Chika <water_chika@outlook.com>
2026-04-13 22:18:09 +0800

    generate_cmd_buf_layer.py: call next layer function

--- generate_cmd_buf_layer.py
+++ generate_cmd_buf_layer.py
@@ -10,24 +10,33 @@ def generate(file, output):
         name = command.find('proto/name')
         params = command.findall('param')
         if (name != None and ret_type != None and params != None and 'cmdbufferlevel' in command.attrib):
+            if name.text == 'vkCmdRefreshObjectsKHR':
+                continue
             print(ret_type.text, name.text, "(", end="", file=output)
             first_param = True;
+            param_names = ""
             for param in params:
                 if not first_param:
                     print(",", file=output)
+                    param_names += ", ";
                 else:
                     print("", file=output)
                     first_param = False
                 print("    ", "".join(param.itertext()).replace("VkRefreshObjectListKHR", "void"), end="", file=output)
+                param_names += param.find("name").text
             print(") {", file=output)
             print("    could_split = false;", file=output)
+            print("    auto next_call = static_cast<PFN_{0}>(parent::get_next_device_proc_addr(\"{0}\"));".format(name.text), file=output)
+            print("    return next_call({});".format(param_names), file=output)
             print("}", file=output)
 
 def generate_cmd_buf(file, output):
     print("#pragma once", file=output)
     print("namespace water_chika {", file=output)
-    print("class cmd_buf_split_info {", file=output)
+    print("template<typename T>", file=output)
+    print("class cmd_buf_split_info : public T{", file=output)
     print("public:", file=output)
+    print("using parent = T;", file=output)
     print("cmd_buf_split_info(bool could_split) : could_split{could_split}{}", file=output)
     generate(file, output)
     print("protected:", file=output)
